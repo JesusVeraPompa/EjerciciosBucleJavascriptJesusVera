65b38d83f27be60143da30c3263f3328394268fe
JesusVeraPompa <merkapompa@gmail.com>
2024-07-17 16:16:51 -0500

    1.2 Ejercicio 2 Completado

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-//  Ejercicio 1
+/*-----------------------------------  Ejercicio 1  --------------------------------*/
 /*Realizar un programa que permita el ingreso de un numero y muestre su tabla de
 multiplicar (Los primeros 10 multiplos).*/
 
@@ -9,7 +9,25 @@ let numero = parseInt(prompt("Ingrese un número:"));
 console.log(`Tabla de multiplicar del número ${numero}`);
 
 for (let i = 1; i <= 10; i++) {
-  console.log(`${numero} x ${i} = ${numero * i}`);
+    console.log(`${numero} x ${i} = ${numero * i}`);
 }
 
+/*-----------------------------------  Ejercicio 2  --------------------------------*/
+/*Realizar un programa que permita el ingreso de numeros los cuales se tienen que ir
+acumulando. El ingreso de datos terminara cuando el usuario ingrese un valor 0.*/
 
+let suma = 0;
+
+do {
+    let numero = parseInt(prompt("Ingrese un número (0 para terminar):"));
+
+    if (numero === 0) {
+        break;
+    }
+
+    suma += numero;
+} while (true);
+
+console.log(`La suma de todos los números ingresados es: ${suma}`);
+
+/*-----------------------------------  Ejercicio 3  --------------------------------*/
